aa305baea7e130bbfa545959b5e2ef284cdc4f6f
ahickco <ahickco@22c924b0-f8b6-492c-bba9-589db0560856>
2008-02-21 16:04:46 +0000

    adding files for oxford experience website and reorganizing for best practices

---
+++ trunk/src/kwic.php
@@ -0,0 +1,71 @@
+<?php
+include_once("config.php");
+include_once("lib/xmlDbConnection.class.php");
+
+$exist_args{"debug"} = true;
+
+$db = new xmlDbConnection($exist_args);
+
+global $title;
+global $abbrev;
+global $collection;
+
+
+$id = $_GET["id"]; 
+$keyword = $_GET["keyword"];
+
+
+$htmltitle = "The Oxford Experience";
+
+
+// what should be displayed here?  for sc: article title, author, date
+
+// use article query with context added
+// note: using |= instead of &= because we want context for any of the
+// keyword terms, whether they appear together or not
+$xquery = "let \$doc := /TEI.2[@id = \"$id\"]
+return 
+<item>
+{\$doc/@id}
+{\$doc//titleStmt//title}
+{\$doc//titleStmt//author/name}
+{\$doc//sourceDesc//date}
+<context>
+{for \$c in \$doc//*[. |= \"$keyword\"]
+   return if (name(\$c) = 'hi') then \$c/..[. |= \"$keyword\"] else  \$c }</context>
+</item>";
+
+
+/* this is one way to specify context nodes  (filter based on the kinds of nodes to include)
+  <context>{(\$a//p|\$a//titlePart|\$a//q|\$a//note)[. &= '$keyword']}</context>
+   above is another way-- allow any node, but if the node is a <hi>, return parent instead
+   (what other nodes would need to be excluded? title? others?)
+*/
+
+$db->xquery($xquery);
+//$doctitle = $db->findnode("title");
+// truncate document title for html header
+//$doctitle = str_replace(", an electronic edition", "", $doctitle);
+
+
+print "$doctype
+<html>
+ <head>
+    <title>$htmltitle : $doctitle : Keyword in Context</title>
+    <link rel='stylesheet' type='text/css' href='oxfexp.css'>";
+
+include("xml/browse-head.xml");
+
+print "<div class='content'>
+<div class='title'><a href='index.html'>$title</a></div>";
+
+$xsl_params = array("url_suffix" => "keyword=$keyword");
+
+$db->xslBind("kwic-towords.xsl");
+$db->xslBind("kwic.xsl", $xsl_params);
+
+$db->transform();
+$db->printResult();
+
+
+?>
